4b2a67045bdb4e686c30ade4fe65065f499fd818
biswajitecd <biswajit@ethicalden.com>
2025-07-24 18:30:26 +0530

    navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["pub-f2300dc39d77486db13ba83d33a85773.r2.dev",],
+  },
+};
 
 export default nextConfig;
