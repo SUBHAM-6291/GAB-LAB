44fa3385862d68abd24a59bbd549daaeb877a092
SUBHAM-6291 <subhamsingh39621@gmail.com>
2025-07-28 18:25:23 +0530

    tell us more here and   and partnership page fix

--- app/_components/Partnership/Partner.jsx
+++ app/_components/Partnership/Partner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Banner from './Banner/Banner'
 import Tailored from './Tailored/Tailored'
-import Partner_with_us from './Partner_with_us/Partner_with_us'
+// import Partner_with_us from './Partner_with_us/Partner_with_us'
 import Benifits from './Benifits/Benifits'
 import Query from './Query/Query'
 import Faq from '../Faq/Faq'
@@ -11,7 +11,7 @@ const Partner = () => {
     <div>
       <Banner/>
       <Tailored/>
-      <Partner_with_us/>
+      {/* <Partner_with_us/> */}
       <Benifits/>
       <Faq/>
       <Query/>
